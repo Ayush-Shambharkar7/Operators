71d6c92327bd25ae3a2dc462dae8ad70494be76e
Ayush-Shambharkar7 <ayushshambharkar0007@gmail.com>
2024-07-29 21:20:33 +0530

    commit message

---
+++ positive_or_negative.cpp
@@ -0,0 +1,26 @@
+//Ayush Shambharkar 
+//23070123034
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    if (a>=0)
+    {
+        cout<<"It is a positive number.";
+    }
+    else
+    {
+        cout<<"It is a negative number.";
+    }
+}
+
+/*Enter a number: 5
+It is a positive number.
+
+Enter a number: -6
+It is a negative number.*/
