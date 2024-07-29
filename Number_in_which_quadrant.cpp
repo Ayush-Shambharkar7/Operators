823f238f3dd67570b601ee8d949979a2c6c7992d
Ayush-Shambharkar7 <ayushshambharkar0007@gmail.com>
2024-07-29 21:18:05 +0530

    commit message

---
+++ Number_in_which_quadrant.cpp
@@ -0,0 +1,39 @@
+//Ayush Shambharkar
+//23070123034
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"Enter a number: ";
+    cin>>b;
+
+    if(a>=0 && b>=0)
+    {
+        cout<<"It is in 1st quadrant.";
+    }
+    else if(a<0 && b>=0)
+    {
+        cout<<"It is in 2nd quadrant.";
+    }
+    else if(a<0 && b<0)
+    {
+        cout<<"It is in 3rd quadrant.";
+    }
+    else
+    {
+        cout<<"It is in 4th quadrant";
+    }
+}
+
+/*Enter a number: 5
+Enter a number: -5
+It is in 4th quadrant
+
+Enter a number: -8
+Enter a number: -9
+It is in 3rd quadrant.*/
